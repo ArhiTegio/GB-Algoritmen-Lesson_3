8a3d069139a6e665610e71b4408b97acc360f473
Arhio <Arhio@yandex.ru>
2019-11-08 19:20:35 +0300

    Алгоритмы. Задание 3. Начальное оформление проекта.

--- GB-Algoritmen-Lesson_3/Program.cs
+++ GB-Algoritmen-Lesson_3/Program.cs
@@ -3,13 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibraryFastDecisions;
+using static System.Console;
+using System.Reflection;
 
 namespace GB_Algoritmen_Lesson_3
 {
     class Program
     {
+        static Dictionary<string, Act> dict = new Dictionary<string, Act>
+        {
+            { "1", new Act },
+        };
+
         static void Main(string[] args)
         {
+            var ex = new Extension();
+            var q = new Questions();
+            var n = "";
+            WriteLine("С# - Алгоритмы и структуры данных. Задание 3.");
+            WriteLine("Кузнецов");
+            while (n != "0")
+            {
+                WriteLine("Введите номер интересующей вас задачи:" + Environment.NewLine +
+                    "1. Попробовать оптимизировать пузырьковую сортировку. Сравнить количество операций сравнения оптимизированной и неоптимизированной программы. Написать функции сортировки, которые возвращают количество операций." + Environment.NewLine +
+                    "2. * Реализовать шейкерную сортировку." + Environment.NewLine +
+                    "3. Реализовать бинарный алгоритм поиска в виде функции, которой передаётся отсортированный массив.Функция возвращает индекс найденного элемента или –1, если элемент не найден." + Environment.NewLine +
+                    "4. * Подсчитать количество операций для каждой из сортировок и сравнить его с асимптотической сложностью алгоритма." + Environment.NewLine +
+                    "0. Нажмите для выхода из программы.");
+
+                n = q.Question<int>("Введите ", new HashSet<char>() { '0', '1', '2', '3', '4' }, true);
+                if (n == "0") break;
+                dict[n].Work();
+            }
+
+            Console.ReadKey();
         }
+    }
+
+    abstract class Act
+    {
+        public abstract void Work();
     }
 }
